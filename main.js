c4b10c816410ef81111f2ebaa1864e561e5c89cb
Jabulile Ndimande <131398578+jabulilendimande@users.noreply.github.com>
2025-06-10 04:13:32 +0200

    adding functionality to the pages

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var deleteBtn = document.getElementById("delete");
 deleteBtn.addEventListener("click", () => {
-    alert("Do you want to delete this user");
+    alert("Delete user");
+    /*DELETE img , name ,username from tablename
+    */
 });
 
 
