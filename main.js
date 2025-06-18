9c946a3b552d6e36818bc89c581dd08aa627f538
Jabulile Ndimande <131398578+jabulilendimande@users.noreply.github.com>
2025-06-19 00:38:48 +0200

    making web to be functional

--- main.js
+++ main.js
@@ -1,34 +1,53 @@
 
 function validate(userInput){  //validate all user input before submit
+    document.getElementById(userInput.id + "-error").innerHTML = "";
     if( !userInput.checkValidity()){
         document.getElementById(userInput.id + "-error").innerHTML = userInput.validationMessage;
         return false;
     }
-    userInput = userInput.value.trim();
+    trimmedValue = userInput.value.trim();
 
     switch (userInput.id){
-        case "name":
+        case "firstname":
+        case "lastname":
             if(!/^[A-Za-z]+$/.test(userInput.value)){
+                document.getElementById(userInput.id + "-error").innerHTML = "Only letters allowed.";
+                return false
                 
             }
             break;
-        case "surname":
-            if(!/^[A-Za-z]+$/.test(userInput.value)){
-
-            }
-            break;
         case "email":
-            if(!userInput.value.includes("@")){
+            if(!trimmedValue.includes("@")){
                 document.getElementById(userInput.id + "-error").innerHTML = "Email must contain @";
                 return false;
             }
             break;
         case "phone":
-            if(!/^\d{10}$/.test(userInput.value)){
+            if(!/^\d{10}$/.test(trimmedValue)){
                 document.getElementById(userInput.id + "-error").innerHTML= "Enter a valid 10 digit phone number";
                 return false;
             }
             break;
     }
     return true;
+}
+
+function validateAll(){
+    const inputs = ["firstname" , "lastname" , "email" , "phone"];
+    let isValid = true;
+    for(let id of inputs){
+        const input = document.getElementById(id);
+        if(!validate(input)){
+            isValid = false;
+        
+        }    
+    }
+    return isValid;
+}
+
+function submitForm(e){
+    if(!validateAll()){
+        alert();
+        return;
+    }
 }
