92bd8119af22e84fc64a3fe182d44175d525ec43
Jabulile Ndimande <131398578+jabulilendimande@users.noreply.github.com>
2025-06-18 19:48:44 +0200

    making functions clickable

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 
+var id = getID();
+function getContact(){
+    fetch(rootPath+'controller/get-contacts/?id='+id)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        displayContactinfo(data);
+    })
+}
+function displayContactinfo(data){
+    avatarImg =`<img src=" ${rootPath}controller/uploads/${data[0].avatar}" width="200"/>`; 
+    document.getElementById("avatarImage").innerHTML = avatarImg;
+    document.getElementById("name").value = `${data[0].firstname}`;
+    document.getElementById("surname").value = `${data[0].lastname}`;
+    document.getElementById("email").value = `${data[0].email}`;
+    document.getElementById("phone").value = `${data[0].mobile}`;
+}
 
-var deleteBtn = document.getElementById("delete");
+const deleteBtn = document.getElementById("delete");
 deleteBtn.addEventListener("click", deleteContact);
 
 function deleteContact(){
@@ -21,40 +39,12 @@ function deleteContact(){
    // fetchContacts();
 }
 
-
+//when home button is clicked , it should use homelink() to direct to index.html
 document.getElementById("homeLink").addEventListener("click",homeLink);
 
-function editContact(id){
-    window.open("edit.html?id=" + id, "_self");
-
+function homeLink(){
+    window.location.href = 'index.html';
 }
-
-var id = getID();
-
-function getID(){
-    var url = window.location.href;
-    var position = url.search("id=");
-    var id = url.slice(position +1);
-    return id;
-}
-function getContact(){
-    fetch(rootPath+'controller/get-contacts/?id='+id)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        displayContactinfo();
-    })
-}
-function displayContactinfo(){
-    avatarImg =`<img src=" ${rootPath}controller/uploads/${data[0].avatar} width="200""/>`; 
-    document.getElementById("avatarImage").innerHTML = avatarImg;
-    document.getElementById("name").value = `${data[0].firstname}`;
-    document.getElementById("surname").value = `${data[0].lastname}`;
-    document.getElementById("email").value = `${data[0].email}`;
-    document.getElementById("phone").value = `${data[0].mobile}`;
-}
-
 //make contact editable 
 document.getElementById("editPage").addEventListener("click",editContacts);
 
@@ -70,7 +60,7 @@ function editContacts(){
     document.getElementById("submitForm-edit").hidden = false;
 
 }
-
+document.getElementById("submitForm-edit").addEventListener("click",submitFormEdit);
 function submitFormEdit(e){
     e.preventDefault(); //prevent reloading
     const formEdit = new FormData(document.querySelector('#editForm'));
@@ -94,7 +84,51 @@ function submitFormEdit(e){
         }
     })
 }
-document.getElementById("submitForm-edit").addEventListener("click",submitFormEdit);
+
+/*Add page */
+
+//add button uses submitForm
+document.getElementById("submitForm-add").addEventListener("click",submitFormAdd);
+
+function submitFormAdd(e){ 
+    e.preventDefault();
+    const formAdd = new FormData(document.querySelector('#main-user'));
+    formAdd.append('apiKey', apiKey);
+    /*submit form data to the server*/
+    fetch(rootPath + 'controller/insert-contact',{
+        method:'POST',
+        headers:{'Accept': 'application/json, *.*'},
+        body: formAdd
+    })
+    .then( function(response){
+        return response.text();
+    })  /* return 1  or 0*/
+    .then(function(data){
+        if(data=="1"){
+            alert("Contact added.");
+            homeLink();
+        }else{
+            alert(data);
+            homeLink();
+        }
+    })
+
+}
+
+function editContact(id){
+    window.open("edit.html?id=" + id, "_self");
+
+}
+
+function getID(){
+    var url = window.location.href;
+    var position = url.search("id=");
+    var id = url.slice(position +1);
+    return id;
+}/*function getID() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get("id");
+}*/
 
 
 function validate(userInput){  //validate all user input before submit
@@ -136,38 +170,6 @@ var refreshBtn  = document.getElementById("refresh-btn");
 refreshBtn.addEventListener("click",fetchContacts);
 
 
-//add button uses submitForm
-document.getElementById("submitForm-add").addEventListener("click",submitFormAdd);
-
-function submitFormAdd(e){ 
-    e.preventDefault();
-    const formAdd = new FormData(document.querySelector('#main-user'));
-    formAdd.append('apiKey', apiKey);
-    /*submit form data to the server*/
-    fetch(rootPath + 'controller/insert-contact',{
-        method:'POST',
-        headers:{'Accept': 'application/json, *.*'},
-        body: formAdd
-    })
-    .then( function(response){
-        return response.text();
-    })  /* return 1  or 0*/
-    .then(function(data){
-        if(data=="1"){
-            alert("Contact added.");
-            homeLink();
-        }else{
-            alert(data);
-            homeLink();
-        }
-    })
-
-}
-
-function homeLink(){
-    window.open("index.html","_self");
-}
-
 /*add api function*/
 function fetchContacts(){
     fetch(rootPath + "controller/get-contacts/")
@@ -184,7 +186,7 @@ function displayOutput(data){ //data is the object
 output = "<table>";
 for(a in data){  /*iterate through object array*/
     output += `
-        <tr onclick="edit.html(${data[a].id})">
+        <tr onclick="editContact(${data[a].id})">
             <td><img src="${rootPath}controller/uploads/${data[a].avatar}" width="40px"/></td>
             <td> <h5> ${data[a].firstname} </h5> </td>
             <td> <h5> ${data[a].lastname} </h5> </td>
@@ -195,41 +197,11 @@ output += "</table>";
 document.getElementById("table").innerHTML = output;
 
 }
-
-
-/*FOR ADD page 
-
-after user clicks submit > run before being posted on the database
-var enterKey = document.getElementById();
-enterKey.addEventListener("keydown",(event)=>{
-    if(event.key ==="Enter"){
-        event.preventDefault();
-        /*create object to take all possible  user inputs
-            name = document.getElementById("name").value.trim()
-            surname = document.getElementById("surname").value.trim()
-            email = document.getElementById("email").value.trim()
-            phone = document.getElementById("phone").value.trim()
-        
-        validate(userInput);
-    }
-});*/
 //button uses onclick
 function addContact(){
     window.open("add.html", "_self");
 }
+/*document.addEventListener("DOMContentLoaded", function () {*/
 
 
-var middle = document.getElementById('middle');
-var upper = document.getElementById('upper');
-
-function getMiddle(){
-    if(grade===middle){
-
-    }
-    fetch()
-    .then(function(response){
-        return response;
-
-    })
-}
 
